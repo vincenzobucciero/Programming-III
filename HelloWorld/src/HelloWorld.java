6ad21f44a94d7d8a52c69d7762845deaa40e0039
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 10:03:04 +0100

    agg comm

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,23 @@
+
+//Qualsiasi applicazione deve sempre avere un metodo main
+
+/*
+Classe HelloWorld pubblica
+ */
+
+/*
+    System -> classe di Object (superclasse)
+    out -> oggetto della classe System
+    println -> metodo della classe
+*/
+
 public class HelloWorld {
-    public static void main(String[] args) {
+    //Metodo main non agisce su un oggetto (sempre static)
+    public static void main(String[] args) {    //(...) argomenti dalla linea di comando
         System.out.println("Hello World!!");
     }
 }
+
+
+//classe -> astrazione indicante un insieme di oggetti che condividono le stesse caratteristiche e le stesse funzionalità
+//oggetto -> istanza (creazione fisica) di una classe
