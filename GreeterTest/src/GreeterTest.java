8314d9c9a78d870e1903d9249df4d4d6d2040095
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 10:31:14 +0100

    test class greeter

---
+++ GreeterTest/src/GreeterTest.java
@@ -0,0 +1,9 @@
+//classe Test per la classe Greeter
+public class GreeterTest {
+    public static void main(String[] args) {
+        //creo oggetto del metodo della classe Greeter
+        Greeter worldGreeter = new Greeter();
+        //utilizzo metodo implementato nella classe Greeter
+        System.out.println(worldGreeter.sayHello());
+    }
+}
