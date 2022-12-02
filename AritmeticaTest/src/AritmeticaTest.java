ffc9c7b2b660f81e05b16ba39203944c264d8837
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 10:30:50 +0100

    test class

---
+++ AritmeticaTest/src/AritmeticaTest.java
@@ -0,0 +1,8 @@
+//classe test per aritmetica
+public class AritmeticaTest {
+    public static void main (String[] args) {
+        Aritmetica sommaOggetto = new Aritmetica();
+        //stampa utilizzando metodo della classe aritmetica
+        System.out.println(sommaOggetto.Somma(5,10));
+    }
+}
