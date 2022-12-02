aea2488a0a96ab19b5934dbf51af2fab8ac67cec
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 11:34:11 +0100

    agg comm test class

--- GreeterTest/src/GreeterTest.java
+++ GreeterTest/src/GreeterTest.java
@@ -2,7 +2,7 @@
 public class GreeterTest {
     public static void main(String[] args) {
         //creo oggetto del metodo della classe Greeter
-        Greeter worldGreeter = new Greeter();
+        Greeter worldGreeter = new Greeter("Vincenzo", "Bucciero", 21);
         //utilizzo metodo implementato nella classe Greeter
         System.out.println(worldGreeter.sayHello());
     }
