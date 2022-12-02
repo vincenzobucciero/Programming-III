3db92055a6eafea4f4e614535fc9160b77b8d9fa
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 10:29:41 +0100

    class Greeter

--- GreeterTest/src/Greeter.java
+++ GreeterTest/src/Greeter.java
@@ -3,7 +3,7 @@
 public class Greeter {
     //dichiarazione metodo interno alla classe
     public String sayHello() {
-        //messaggio da ritornare di tipo stringa
+        //messaggio da ritornare di tipo stringa String message = "Ciao Vincenzo !"
         //Ritorno del metodo
         return "Ciao Vincenzo !";
     }
