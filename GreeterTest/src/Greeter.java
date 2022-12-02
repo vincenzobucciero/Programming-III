521abb182d5c728e0ade76fcd63a2502a34b9bb8
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 11:33:55 +0100

    agg comm class

--- GreeterTest/src/Greeter.java
+++ GreeterTest/src/Greeter.java
@@ -1,4 +1,5 @@
 
+/*
 //classe pubblica non contenente il main
 public class Greeter {
     //dichiarazione metodo interno alla classe
@@ -8,3 +9,26 @@ public class Greeter {
         return "Ciao Vincenzo !";
     }
 }
+*/
+
+//modifica classe Greeter con utilizzo delle variabili istanza
+public class Greeter {
+    //Variabile istanza
+    private final String name;
+    private final String surname;
+    private final int eta;
+    //utilizzo del costruttore
+    public Greeter (String mioNome, String mioCognome, int miaEta) {
+        name = mioNome;
+        surname = mioCognome;
+        eta = miaEta;
+    }
+    public String sayHello() {
+        return "Ciao " + name + " " + surname + " " + eta;
+    }
+}
+
+//costruttore -> Inizializza gli oggetti, generalmente dichiarati public
+//costruttori + metodi = interfaccia pubblica
+
+//VAriabile istanza -> generalmenete dichiarate private, solo i metodi della classe possono accedervi, INCAPSULAMENTO.
