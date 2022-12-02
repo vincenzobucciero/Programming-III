5174bd9d3df8618d8a9eb940636b035261fa71ed
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 11:36:07 +0100

    class test varargs

---
+++ AritmVarArgs/src/AritmVarArgsTest.java
@@ -0,0 +1,9 @@
+//class Test
+public class AritmVarArgsTest {
+    public static void main (String[] args) {
+        AritmVarArgs sumVarArgs = new AritmVarArgs();
+        System.out.println(sumVarArgs.Somma(1,2,3));
+        System.out.println(sumVarArgs.Somma(2,10));
+        System.out.println(sumVarArgs.Somma(10));
+    }
+}
