749a5e4f9ac6abf3a604d3c3e9e9cf087db394a8
vincenzobucciero <vincenzo.bucciero001@studenti.uniparthenope.it>
2022-12-02 11:37:34 +0100

    agg class BankAccount

---
+++ BankAccount/src/BankAccount.java
@@ -0,0 +1,37 @@
+/*
+    Implementare una classe BankAccount che permette di gestire
+    un conto bancario contenente un saldo che può essere modificato
+    da depositi e prelievi
+        costruttore
+        metodo deposit per versare denaro nel conto bancario
+        metodo withdraw preleva denaro dal conto bancario
+        metodo getBalance ritorna il saldo attuale
+ */
+
+public class BankAccount {
+    private double balance;
+    public BankAccount (double mbalance) {
+        //this -> parola chiave per accedere al parametro implicito
+        this.balance = mbalance;
+    }
+
+    //OVERLOADING -> piu metodi con lo stesso nome
+    //Inizializzo conto bancario a 0
+    public BankAccount () {
+        this(0);
+    }
+
+    public void deposit (double amount) {
+        // balance -> parametro implicito, amount -> parametro esplicito
+        //variabile inline
+        this.balance = this.balance + amount;
+    }
+    public void withdraw (double amount) {
+        //variabile inline
+        this.balance = this.balance - amount;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
